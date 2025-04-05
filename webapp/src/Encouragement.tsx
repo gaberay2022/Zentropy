27cd5f3a4ae0506f8878223331e41ea6afb1e1c9
Phillip Drake <118027787+Phillip-Drake@users.noreply.github.com>
2025-04-05 10:10:52 -0500

    Some Progress
    TODO :
    
    Create Encouragement Display
    
    Finish up some details on canvas page

--- webapp/src/Encouragement.tsx
+++ webapp/src/Encouragement.tsx
@@ -1,7 +1,25 @@
 import "./Encouragement.css"
-function Encouragement() {
-    return (<div className={"EncouragementWrapper"}>
 
+import encouragementCat from "/svgs/Encouragement_cat.svg"
+
+function Encouragement() {
+
+
+
+    function triggerAnimation(){
+        const encouragement_1 = document.getElementById("encouragement_1")
+        if(encouragement_1){
+            encouragement_1.classList.add("RemoveEncouragement")
+        }
+
+    }
+
+    return (
+        <div className={"EncouragementWrapper"}>
+            <button onClick={() => {triggerAnimation()}}>
+                Temp
+            </button>
+            <img className={"Encouragement"} src={encouragementCat} alt="Encouragement" id="encouragement_1"/>
 
         </div>)
 }
